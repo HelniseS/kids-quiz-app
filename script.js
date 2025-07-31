947e3a9ac454fe0b26b6b9d0190956e481f71be5
HelniseS <helnise-santos12@hotmail.com>
2025-07-31 21:35:55 +0100

    added animals images to the animals quenstions

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const quizzes = {
     Animals: [
-        { question: "Which animal says 'moo'?", image: , options: ["Cow", "Cat", "Sheep"], answer: "Cow"},
-        { question: "Which animal barks?", image: , options: ["Fish", "Dog", "Pig"], answer: "Dog"},
-        { question: "Who is the king of the jumgle?", image: , options: ["Tiger", "Bear", "Lion"], answer: "Lion"},
-        { question: "Which animal lives in water?", image: , options: ["Frog", "Fish", "Bird"], answer: "Fish"},
-        { question: "Which animal says 'meow'?", image: , options: ["Chicken", "Cat", "Horse"], answer: "Cat"},
-        { question: "Which one flies?", image: , options: ["Penguin", "Girafe", "Bird"], answer: "Bird"},
-        { question: "Which animal hops?", image: , options: ["Elephant", "Kangaroo", "Monkey"], answer: "Kangaroo"},
-        { question: "Which animal has a trunk?", image: , options: ["Cow", "Lion", "Elephant"], answer: "Elephant"},
-        { question: "Which animal lays eggs?", image: , options: ["Hen", "Goat", "Dog"], answer: "Hen"},
-        { question: "Fastest land animal?", image: , options: ["Zebra", "Horse", "Cheetan"], answer: "Cheetan"},
+        { question: "Which animal says 'moo'?", image: "assets\images\cow.png", options: ["Cow", "Cat", "Sheep"], answer: "Cow"},
+        { question: "Which animal barks?", image: "assets/images/dog.png", options: ["Fish", "Dog", "Pig"], answer: "Dog"},
+        { question: "Who is the king of the jumgle?", image: "assets/images/lion.png", options: ["Tiger", "Bear", "Lion"], answer: "Lion"},
+        { question: "Which animal lives in water?", image:"assets/images/fish.png", options: ["Frog", "Fish", "Bird"], answer: "Fish"},
+        { question: "Which animal says 'meow'?", image: "assets/images/cat.png", options: ["Chicken", "Cat", "Horse"], answer: "Cat"},
+        { question: "Which one flies?", image: "assets/images/bird.png", options: ["Penguin", "Girafe", "Bird"], answer: "Bird"},
+        { question: "Which animal hops?", image: "assets/images/kangaroo.png", options: ["Elephant", "Kangaroo", "Monkey"], answer: "Kangaroo"},
+        { question: "Which animal has a trunk?", image: "assets/images/elephant.png", options: ["Cow", "Lion", "Elephant"], answer: "Elephant"},
+        { question: "Which animal lays eggs?", image: "assets/images/hen.png", options: ["Hen", "Goat", "Dog"], answer: "Hen"},
+        { question: "Fastest land animal?", image: "assets/images/cheetah.png", options: ["Zebra", "Horse", "Cheetah"], answer: "Cheetah"},
     ]
     }
