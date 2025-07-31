2c8099d2725a1c991640549beed306036ef33500
HelniseS <helnise-santos12@hotmail.com>
2025-07-31 21:06:40 +0100

    Added  10  questions , and answer about animals

---
+++ script.js
@@ -0,0 +1,14 @@
+const quizzes = {
+    Animals: [
+        { question: "Which animal says 'moo'?", image: , options: ["Cow", "Cat", "Sheep"], answer: "Cow"},
+        { question: "Which animal barks?", image: , options: ["Fish", "Dog", "Pig"], answer: "Dog"},
+        { question: "Who is the king of the jumgle?", image: , options: ["Tiger", "Bear", "Lion"], answer: "Lion"},
+        { question: "Which animal lives in water?", image: , options: ["Frog", "Fish", "Bird"], answer: "Fish"},
+        { question: "Which animal says 'meow'?", image: , options: ["Chicken", "Cat", "Horse"], answer: "Cat"},
+        { question: "Which one flies?", image: , options: ["Penguin", "Girafe", "Bird"], answer: "Bird"},
+        { question: "Which animal hops?", image: , options: ["Elephant", "Kangaroo", "Monkey"], answer: "Kangaroo"},
+        { question: "Which animal has a trunk?", image: , options: ["Cow", "Lion", "Elephant"], answer: "Elephant"},
+        { question: "Which animal lays eggs?", image: , options: ["Hen", "Goat", "Dog"], answer: "Hen"},
+        { question: "Fastest land animal?", image: , options: ["Zebra", "Horse", "Cheetan"], answer: "Cheetan"},
+    ]
+    }
